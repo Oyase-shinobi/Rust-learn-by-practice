c5b82c16c5105eeb47b389bb3b6bcd9248653f38
Oyase-shinobi <donjosy305@gmail.com>
2023-06-29 21:25:17 +0100

    state3

--- src/basic_types/statements_and_expressions/statements_and_expressions3.rs
+++ src/basic_types/statements_and_expressions/statements_and_expressions3.rs
@@ -11,14 +11,12 @@
 // }
 
 
-
 fn main() {
     let s = sum(1 , 2);
     assert_eq!(s, 3);
 
     println!("Success!");
 }
-
 fn sum(x: i32, y: i32) -> i32 {
     x + y
     // i removed the semicolon to make it an expression, then it will return a value
