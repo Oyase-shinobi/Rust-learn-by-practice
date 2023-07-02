ef5f4bca71b0f3c9779a574fbaa636223bd0f41f
Oyase-shinobi <donjosy305@gmail.com>
2023-07-02 16:42:03 +0100

    utf8 string

---
+++ src/compound_types/Strings/mod.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code, unused_variables, unused_assignments)]
+mod string1;
+mod string2;
+mod string3;
+mod string4;
+mod string5;
+mod string6;
+mod string7;
+mod string8;
+mod string9;
+mod string10;
+mod string11;
+mod string12;
