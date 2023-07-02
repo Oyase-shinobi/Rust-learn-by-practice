cb85173e8d139bdc8ae9c89833f6e6df756010d5
Oyase-shinobi <donjosy305@gmail.com>
2023-07-02 16:46:01 +0100

    Arrays

---
+++ src/compound_types/Array/array4.rs
@@ -0,0 +1,16 @@
+
+// fn main() {
+//     // Fix the error
+//     let _arr = [1, 2, '3'];
+
+//     println!("Success!");
+// }
+
+
+// All elements in an array must be of the same type
+fn main() {
+    // Fix the error
+    let _arr = [1, 2, 3];
+
+    println!("Success!");
+}
