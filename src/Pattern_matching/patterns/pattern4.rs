470fff6442c7ed88ab5d18ccad129d50c2d6d2bd
Oyase-shinobi <donjosy305@gmail.com>
2023-07-06 13:13:02 +0100

    patterns

---
+++ src/Pattern_matching/patterns/pattern4.rs
@@ -0,0 +1,29 @@
+
+// // Fill in the blank to make the code work, `split` MUST be used
+// fn main() {
+//     let num = Some(4);
+//     let split = 5;
+//     match num {
+//         Some(x) __ => assert!(x < split),
+//         Some(x) => assert!(x >= split),
+//         None => (),
+//     }
+
+//     println!("Success!");
+// }
+
+
+// A match guard is an additional if condition specified after 
+// the pattern in a match arm that must also match, along with 
+// the pattern matching, for that arm to be chosen.
+fn main() {
+    let num = Some(4);
+    let split = 5;
+    match num {
+        Some(x) if x < split => assert!(x < split),
+        Some(x) => assert!(x >= split),
+        None => (),
+    }
+
+    println!("Success!");
+}
---
+++ src/Pattern_matching/patterns/pattern6.rs
@@ -0,0 +1,24 @@
+
+// // FIX the error with least changing
+// // DON'T remove any code line
+// fn main() {
+//     let mut v = String::from("hello,");
+//     let r = &mut v;
+
+//     match r {
+//        &mut value => value.push_str(" world!") 
+//     }
+// }
+
+
+
+//  Using pattern &mut V to match a mutable reference requires 
+// you to be very careful, due to V being a value after matching.
+fn main() {
+    let mut v = String::from("hello,");
+    let r = &mut v;
+
+    match r {
+       value => value.push_str(" world!") 
+    }
+}
