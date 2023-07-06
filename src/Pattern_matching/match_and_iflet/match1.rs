6ae0fa222a3acbdea7ab8784554e8ea991061e47
Oyase-shinobi <donjosy305@gmail.com>
2023-07-06 13:08:19 +0100

    match

---
+++ src/Pattern_matching/match_and_iflet/match1.rs
@@ -0,0 +1,40 @@
+
+// // Fill the blanks
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+
+// fn main() {
+//     let dire = Direction::South;
+//     match dire {
+//         Direction::East => println!("East"),
+//         __  => { // Matching South or North here
+//             println!("South or North");
+//         },
+//         _ => println!(__),
+//     };
+// }
+
+
+
+// Fill the blanks
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::North| Direction::South  => { // Matching South or North here
+            println!("South or North");
+        },
+        _ => println!("Direction is West"),
+    };
+}
