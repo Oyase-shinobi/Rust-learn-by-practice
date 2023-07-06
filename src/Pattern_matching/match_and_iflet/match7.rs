95bb5ea17989c653276dabf79268931acad80578
Oyase-shinobi <donjosy305@gmail.com>
2023-07-06 13:09:27 +0100

    if let

---
+++ src/Pattern_matching/match_and_iflet/match7.rs
@@ -0,0 +1,31 @@
+
+// // Fill in the blank
+// enum Foo {
+//     Bar(u8)
+// }
+
+// fn main() {
+//     let a = Foo::Bar(1);
+
+//     __ {
+//         println!("foobar holds the value: {}", i);
+
+//         println!("Success!");
+//     }
+// }
+
+
+// Fill in the blank
+enum Foo {
+    Bar(u8)
+}
+
+fn main() {
+    let a = Foo::Bar(1);
+
+    if let Foo::Bar(i) = a {
+        println!("foobar holds the value: {}", i);
+
+        println!("Success!");
+    }
+}
