a0d14f554f7691239eb73b219337afd3c0eb7fa2
Oyase-shinobi <donjosy305@gmail.com>
2023-07-14 21:55:34 +0100

    capacity

---
+++ src/collectionTypes/Strings/string7.rs
@@ -0,0 +1,53 @@
+
+// // FILL in the blanks
+// use std::mem;
+
+// fn main() {
+//     let story = String::from("Rust By Practice");
+
+//     // Prevent automatically dropping of the String's data
+//     let mut story = mem::ManuallyDrop::new(story);
+
+//     let ptr = story.__();
+//     let len = story.__();
+//     let capacity = story.__();
+
+//     assert_eq!(16, len);
+
+//     // We can rebuild a String out of ptr, len, and capacity. This is all
+//     // unsafe because we are responsible for making sure the components are
+//     // valid:
+//     let s = unsafe { String::from_raw_parts(ptr, len, capacity) };
+
+//     assert_eq!(*story, s);
+
+//     println!("Success!");
+// }
+
+
+
+// FILL in the blanks
+use std::mem;
+
+fn main() {
+    let story = String::from("Rust By Practice");
+
+    // Prevent automatically dropping of the String's data
+let mut story = mem::ManuallyDrop::new(story);
+
+let ptr = story.as_ptr();
+let len = story.len();
+let capacity = story.capacity();
+
+assert_eq!(16, len);
+
+// We can rebuild a String out of ptr, len, and capacity. This is all
+// unsafe because we are responsible for making sure the components are
+// valid:
+let s = unsafe { String::from_raw_parts(ptr as *mut _, len, capacity) };
+
+assert_eq!(*story, s);
+
+println!("Success!");
+
+}
