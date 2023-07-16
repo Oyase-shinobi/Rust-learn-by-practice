490b38b40d8b027c0a9e1c7c5395f6f4fe158866
Oyase-shinobi <donjosy305@gmail.com>
2023-07-16 13:07:33 +0100

    modules

---
+++ src/collectionTypes/Vectors/mod.rs
@@ -0,0 +1,9 @@
+#![allow(dead_code, unused_variables, unused_assignments)]
+mod vector1;
+mod vector2;
+mod vector3;
+mod vector4;
+mod vector5;
+mod vector6;
+mod vector7;
+mod vector8;
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ mod compound_types;
 mod Flow_Control;
 mod Pattern_matching;
 mod Method_And_AssFunction;
+mod collectionTypes;
+mod Generics_and_Traits;
+mod type_conversion;
+mod result_panic;
 fn main() {}
