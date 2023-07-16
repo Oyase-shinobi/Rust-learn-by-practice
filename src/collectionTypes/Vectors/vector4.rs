78989fab0631fb20a9b4bfebb0c17f17580c219a
Oyase-shinobi <donjosy305@gmail.com>
2023-07-16 13:01:39 +0100

    Indexing

---
+++ src/collectionTypes/Vectors/vector4.rs
@@ -0,0 +1,36 @@
+
+// // FIX the error and IMPLEMENT the code
+// fn main() {
+//     let mut v = Vec::from([1, 2, 3]);
+//     for i in 0..5 {
+//         println!("{:?}", v[i])
+//     }
+
+//     for i in 0..5 {
+//        // IMPLEMENT the code here...
+//     }
+    
+//     assert_eq!(v, vec![2, 3, 4, 5, 6]);
+
+//     println!("Success!");
+// }
+
+
+// FIX the error and IMPLEMENT the code
+fn main() {
+    let mut v = Vec::from([1, 2, 3]);
+    for i in 0..5 {
+        println!("Iteration #{i}: {:?}", v.get(i))
+    }
+
+    for i in 0..5 {
+        match v.get(i) {
+            Some(e) => v[i] = e + 1,
+            None => v.push(i + 2),
+        }
+    }
+
+    assert_eq!(format!("{:?}", v), format!("{:?}", vec![2, 3, 4, 5, 6]));
+
+    println!("Success!")
+}
