4518df3ea6943486a4f73a3bfedb54120156e05c
Oyase-shinobi <donjosy305@gmail.com>
2023-07-17 15:33:00 +0100

    map & and_then

---
+++ src/result_panic/result/result4.rs
@@ -0,0 +1,29 @@
+// use std::num::ParseIntError;
+
+// // FILL in the blank in two ways: map, and then
+// fn add_two(n_str: &str) -> Result<i32, ParseIntError> {
+//    n_str.parse::<i32>().__
+// }
+
+// fn main() {
+//     assert_eq!(add_two("4").unwrap(), 6);
+
+//     println!("Success!");
+// }
+
+
+// map and and_then are two common combinators for Result<T, E> (also for Option<T>).
+
+use std::num::ParseIntError;
+
+// FILL in the blank in two ways: map, and then
+fn add_two(n_str: &str) -> Result<i32, ParseIntError> {
+    //n_str.parse::<i32>().map(|n| n + 2)
+    n_str.parse::<i32>().and_then(|n| Ok(n + 2))
+}
+
+fn main() {
+    assert_eq!(add_two("4").unwrap(), 6);
+
+    println!("Success!");
+}
