0b67e85c7db946e882ad856e1a2113077e58ba04
Oyase-shinobi <donjosy305@gmail.com>
2023-07-18 13:48:30 +0100

    println!

---
+++ src/formated_output/println_and_format/pf2.rs
@@ -0,0 +1,16 @@
+
+// fn main() {
+//     /* Fill in the blanks to make it print:
+//     Hello world, I am 
+//     Sunface!
+//     */
+//     __("hello world, ");
+//     __("I am");
+//     __("Sunface!");
+//  }
+ 
+fn main() {
+    print!("Hello world, ");
+    print!("I am ");
+    println!("Sunface!");
+}
