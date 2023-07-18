b10d4300a077967df345be2935defa276a0f594b
Oyase-shinobi <donjosy305@gmail.com>
2023-07-18 13:52:18 +0100

    padding with string

---
+++ src/formated_output/formating/formating5.rs
@@ -0,0 +1,25 @@
+// fn main() {
+//     println!("Hello {:5}!", 5); // => Hello     5!
+//     println!("Hello {:+}!", 5); // =>  Hello +5!
+//     println!("Hello {:05}!", 5); // => Hello 00005!
+//     println!("Hello {:05}!", -5); // => Hello -0005!
+
+//     /* Fill in the blank */
+//     assert!(format!("{number:0>width$}", number=1, width=6) == __);
+    
+//     println!("Success!")
+// ;}
+
+
+// You can pad numbers with extra zeros.
+fn main() {
+    println!("Hello {:5}!", 5); // => Hello     5!
+    println!("Hello {:+}!", 5); // =>  Hello +5!
+    println!("Hello {:05}!", 5); // => Hello 00005!
+    println!("Hello {:05}!", -5); // => Hello -0005!
+
+    /* Fill in the blank */
+    assert!(format!("{number:0>width$}", number = 1, width = 6) == "000001");
+
+    println!("Success!");
+}
