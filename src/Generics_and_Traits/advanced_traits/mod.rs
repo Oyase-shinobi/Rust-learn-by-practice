eb70dd9ea2bcd195dbd6d988c573b5dd8d87d479
Oyase-shinobi <donjosy305@gmail.com>
2023-08-11 23:11:19 +0100

    modules

---
+++ src/Generics_and_Traits/advanced_traits/mod.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code, unused_variables, unused_attributes)]
+mod adv_traits1;
+mod adv_traits2;
+mod adv_traits3;
+mod adv_traits4;
+mod adv_traits5;
---
+++ src/Generics_and_Traits/trait_object/mod.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code, unused_variables, unused_parens, unused_imports)]
+mod trait_obj1;
+mod trait_obj2;
+mod trait_obj3;
+mod trait_obj4;
+mod trait_obj5;
